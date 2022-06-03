030b5f9c5dde93eb498b849c0ff493e531cbb511
Vittorio Adesso <adessovittorio8@gmail.com>
2022-06-03 15:51:09 +0200

    added new models & implement an array of objetc to paint with ngFor

--- src/app/components/main/betta-characteristics/betta-characteristics.component.ts
+++ src/app/components/main/betta-characteristics/betta-characteristics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Icharacteristic } from '../models/Imain';
 
 @Component({
   selector: 'app-betta-characteristics',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BettaCharacteristicsComponent implements OnInit {
 
-  constructor() { }
+public bettaCharacteristics: Icharacteristic; 
+  constructor() { 
+    this.bettaCharacteristics = {
+      box :
+      [ 
+        { 
+        title: "Características del pez betta", 
+        text: "Aunque no es un pez especialmente delicado, sí posee cierta sensibilidad, por lo que sus cuidados deberán ceñirse a sus características. Los peces betta pertenecen a la subespecie de los laberíntidos, que significa que respiran oxígeno del aire a través de un órgano llamado laberinto. Y no se trata de una respiración parcial o secundaria, sino que el pez betta necesita salir a respirar para vivir. El sistema de respiración doble del pez betta le obliga a sacar la cabeza del agua y tomar aire, boqueando, de ahí que sea importante que elijas un modelo de pecera con tapa para evitar accidentes (recuerda separarla un par de centímetros del agua para que pueda respirar), ya que tiene cierta tendencia a saltar para salir a coger oxígeno."}
+        , 
+        { title: "Cómo nada un pez pez betta", 
+        text: "Para disfrutar de ese efecto hipnótico y especialmente visual de las peceras, nada mejor que introducir un pez betta. Sin duda, no vas a encontrarte al Michael Phelps de las peceras, ya que su forma de nadar es lenta debido a sus particulares aletas. En contra de lo que pueda parecer, el manto de sus aletas dificulta su desplazamiento y no le sirve para alcanzar grandes velocidades, así que no es extraño que lo encuentres parado sobre alguna planta o incluso apoyado en el fondo."
+        }
+      ],
+      remember: " Recuerda!!! su dificultad para nadar lo hace especialmente torpe ante grandes dimensiones o corrientes, así que elige un filtro de agua de baja potencia para que las burbujas o el efecto del chorro no lo moleste demasiado."
+    }
+    
+
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/components/main/models/Imain.ts
@@ -0,0 +1,14 @@
+export interface Icharacteristic {
+    box : Itypes[];
+    remember: string;  
+}
+
+export interface Itypes {
+    title: string; 
+    text: string;
+}
+
+export interface Ihabitat{
+    title: string; 
+    text: string;
+}
