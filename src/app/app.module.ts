3098b05f4022db41d1dd0090f99c74f048e74b59
Vittorio Adesso <adessovittorio8@gmail.com>
2022-06-06 13:54:39 +0200

    import module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
@@ -9,6 +10,9 @@ import { GalleryComponent } from './components/hero/gallery/gallery.component';
 import { MainComponent } from './components/main/main.component';
 import { BettaCharacteristicsComponent } from './components/main/betta-characteristics/betta-characteristics.component';
 import { BettaHabitatComponent } from './components/main/betta-habitat/betta-habitat.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExtrasComponent } from './components/extras/extras.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +22,9 @@ import { BettaHabitatComponent } from './components/main/betta-habitat/betta-hab
     GalleryComponent,
     MainComponent,
     BettaCharacteristicsComponent,
-    BettaHabitatComponent
+    BettaHabitatComponent,
+    FooterComponent,
+    ExtrasComponent,
   ],
   imports: [
     BrowserModule,
