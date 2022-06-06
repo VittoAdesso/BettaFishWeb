9738abf1554a39771fb2cd75ddb33ec173b52e25
Vittorio Adesso <adessovittorio8@gmail.com>
2022-06-06 13:53:24 +0200

    added new route & path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExtrasComponent } from './components/extras/extras.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GalleryComponent } from './components/hero/gallery/gallery.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { BettaCharacteristicsComponent } from './components/main/betta-characteristics/betta-characteristics.component';
+import { BettaHabitatComponent } from './components/main/betta-habitat/betta-habitat.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'origin', component: HeroComponent },
+  { path: 'characters', component: BettaCharacteristicsComponent },
+  { path: 'home', component: BettaHabitatComponent },
+  { path: 'curioso', component: ExtrasComponent },
+
+  { path: 'gallery', component: GalleryComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
