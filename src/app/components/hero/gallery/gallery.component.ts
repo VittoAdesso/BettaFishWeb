52d6f64bda0bc4562d7198f2a790a3263d5613a1
Vittorio Adesso <adessovittorio8@gmail.com>
2022-06-08 17:28:28 +0200

    added new features & styles & improve design & responsive in general components

--- src/app/components/hero/gallery/gallery.component.ts
+++ src/app/components/hero/gallery/gallery.component.ts
@@ -10,7 +10,7 @@ export class GalleryComponent implements OnInit {
 public galleryContent : Igallery; 
   constructor() {
     this.galleryContent = {
-      imgGallery : ['https://descubrir.online/wp-content/uploads/2022/03/Pez-Betta-2.jpg','https://www.plantas-acuario.es/wp-content/uploads/2019/09/betta-de-doble-cola.jpg', 'http://pecesdelmar.com/wp-content/uploads/2020/10/Pez-betta.jpg', 'https://cuantoviven.b-cdn.net/wp-content/uploads/2021/02/439-nombres-asombrosos-de-peces-betta.jpg', 'https://atlasanimal.com/wp-content/uploads/2021/02/pez-betta.jpg.webp', 'https://img.freepik.com/foto-gratis/pez-luchador-rojo-azul-betta-saimese-aletas-onduladas-fondo-negro_127090-1481.jpg', 'https://verdecora.es/blog/wp-content/uploads/2020/03/tipos-pez-betta-cola-rosa.jpg', 'https://media.nidux.net/pull/2000/2000/14358/455-product-60c63f57844ee-bettas-media-luna.jpg' ]
+      imgGallery : ['https://img.freepik.com/foto-gratis/pez-luchador-rojo-azul-betta-saimese-aletas-onduladas-fondo-negro_127090-1481.jpg','https://www.plantas-acuario.es/wp-content/uploads/2019/09/betta-de-doble-cola.jpg', 'http://pecesdelmar.com/wp-content/uploads/2020/10/Pez-betta.jpg', 'https://verdecora.es/blog/wp-content/uploads/2020/03/tipos-pez-betta-cola-rosa.jpg','https://cuantoviven.b-cdn.net/wp-content/uploads/2021/02/439-nombres-asombrosos-de-peces-betta.jpg',   'https://i.pinimg.com/originals/7e/33/49/7e3349402d17fe0a097da22975bbf592.jpg']
 
     }
   }
@@ -19,3 +19,4 @@ public galleryContent : Igallery;
   }
 
 }
+
--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -12,8 +12,8 @@ public heroContent : Ibeta;
   constructor() { 
     this.heroContent = {
       title: 'Pez Betta', 
-      history: 'Si deseas un pez bonito, llamativo y elegante, no busques más, el pez Betta (Betta splendens) es lo que necesitas en tu hogar. Su nombre proviene de un clan de guerreros antiguos que se conocían por el nombre de Bettah; esto se debe a que la especie, especialmente los machos, pueden llegar a ser muy violentos entre ellos, de ahí que uno de sus sobrenombres sea ‘luchador de Siam’. Por este motivo, es importante conocer los cuidados y recomendaciones sobre esta especie antes de añadirla a nuestro acuario.',
-      img: ['https://img.freepik.com/foto-gratis/pez-betta-media-luna-dorada-sobre-negro-tailandia-lucha-contra-peces-color-dorado-sobre-negro-aislado_192913-473.jpg?w=2000', 'https://verdecora.es/blog/wp-content/uploads/2020/03/tipos-pez-betta-media-luna.jpg']
+      history: 'Su nombre proviene de un clan de guerreros antiguos que se conocían por el nombre de Bettah; esto se debe a que la especie, especialmente los machos, pueden llegar a ser muy violentos entre ellos, de ahí que uno de sus sobrenombres sea ‘luchador de Siam’. Por este motivo, es importante conocer los cuidados y recomendaciones sobre esta especie antes de añadirla a nuestro acuario. Si deseas un pez bonito, llamativo y elegante, no busques más, el pez Betta (Betta splendens) es lo que necesitas en tu hogar. ',
+      img: ['https://th.bing.com/th/id/R.d2e69e1da3aff51a23ab9ae715aeefad?rik=bkr5RGbWkfxNPg&pid=ImgRaw&r=0&sres=1&sresct=1']
     }
   }
 
